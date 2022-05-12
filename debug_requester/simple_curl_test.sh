ee0e6feba50b528ab96a77d85f8ff320313ac950
AngeloGalav <tekken342@gmail.com>
2022-05-12 16:31:14 +0000

    added devcontainer, progress on VOMS support
    Added the devcontainer, which allows the user to
    develop the application using the enviroment
    of a specified container in devcontainer
    Progress was made in VOMS support, you can find some of the commands used
    inside the debug requester.
    Also, some lines on the nginx.conf were commented, and will remain as such
    until further research.

--- debug_requester/simple_curl_test.sh
+++ debug_requester/simple_curl_test.sh
@@ -1,1 +1,8 @@
-curl --cert "../voms-client/certs/x509up_u1000" --key "../voms-client/certs/x509up_u1000" --cacert "../voms-client/certs/user.cert.pem"  "https://pippocnaf.test.example:8081/" 
+curl -k --cert "../voms-client/certs/x509up_u1000" --key "../voms-client/certs/x509up_u1000" --cacert "../voms-client/certs/user.cert.pem" --capath "../nginx"  "https://pippocnaf.test.example:8081/" 
+
+# curl --cert "/tmp/x509up_u1000" --key "/tmp/x509up_u1000"  --capath "/etc/grid-security/certificates"  "https://pippocnaf.test.example:8081/"
+
+# comandi provati:
+# curl --cert "/tmp/x509up_u1000"  --capath "/etc/grid-security/certificates"  --cacert "voms-client/certs/user.cert.pem" "https://pippocnaf.test.example:8081/" 
+# curl --cert "/tmp/x509up_u1000"  --capath "/etc/grid-security/certificates"  "https://pippocnaf.test.example:8081/" 
+# (quest'ultimo provato siccome il proxy voms contiene il certificato dell'utente)
