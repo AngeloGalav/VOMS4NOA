a3edcb919a6f6ff59376536989803ff78d8281bd
AngeloGalav <tekken342@gmail.com>
2022-05-25 13:58:46 +0000

    add more proxy-voms support
    Renamed files to have a more self-explanatory name. Added voms proxy
    recognition in auth_engine. Much work is still needed tho.

--- nginx_docker_revproxy/scripts/auth_engine.js
+++ nginx_docker_revproxy/scripts/auth_engine.js
@@ -3,10 +3,21 @@ function authorize_operation(r) {
     // dati da inviare a OPA
     let opa_data = {
         "operation" : r.headersIn["X-Operation"],
-        "role" : "/" + r.headersIn["X-Role"],
-        "uses_jwt" : r.headersIn["X-EnableJWT"],
-        "token" : r.headersIn["Authorization"]
+        "role" : "/" + r.headersIn["X-Role"],    // ho dovuto aggiungere lo slash siccome il token ha gruppo /dev 
+        "uses_jwt" : r.headersIn["X-EnableJWT"], // quindi ora tutti i gruppi sono nella forma "/""
+        "token" : r.headersIn["Authorization"]   // + nome gruppo.
+    } ;
+    
+    if (r.headersIn["X-VOMS-user"] != "") {
+        // "voms_user" : r.headersIn["X-VOMS-user"],
+        // "voms_vo" : r.headersIn["X-VOMS-VO"]
+        r.log(JSON.stringify(r.headersIn));
+        r.log(JSON.stringify(opa_data));
+        r.log(JSON.stringify(r.variables))
     }
+    
+    opa_data["voms_user"] = r.headersIn["X-VOMS-user"]; 
+    r.log("[DEBUG REVPROXY]: DATA IS " +  opa_data["voms_user"]);
 
     // pacchetto HTTP da inviare ad
     var opts = {
