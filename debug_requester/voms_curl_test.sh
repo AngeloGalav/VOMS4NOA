2099135b642795509dc20a967c372b79d024e143
AngeloGalav <tekken342@gmail.com>
2022-09-25 11:54:33 +0200

    Change server name

--- debug_requester/voms_curl_test.sh
+++ debug_requester/voms_curl_test.sh
@@ -1,6 +1,6 @@
 # WARNING!!! These commands are meant to be used in the devcontainer enviroment. 
 # If you run them on your machine, it wont be able to find the certificates properly.
 
-curl --cert "/tmp/x509up_u1000"  --capath "/etc/grid-security/certificates"  --cacert "nginx_docker_revproxy/certificates_for_https/user.cert.pem" "https://servicecnaf.test.example:8081/operation/" -H "X-Role: $1" -H "X-Operation: $2" -H "X-EnableJWT: false" 
+curl --cert "/tmp/x509up_u1000"  --capath "/etc/grid-security/certificates"  --cacert "nginx_docker_revproxy/certificates_for_https/user.cert.pem" "https://storm-tape.test.example:8081/operation/" -H "X-Role: $1" -H "X-Operation: $2" -H "X-EnableJWT: false" 
 #`ricordati di creare il proxy VOMS con il comando:
 # voms-proxy-init -pwstdin -voms test.vo <<< pass
