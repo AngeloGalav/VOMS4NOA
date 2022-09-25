216beb8844c8a9cb18e3aa1f3f333d1e411e72b0
AngeloGalav <tekken342@gmail.com>
2022-09-25 11:59:13 +0000

    Add full API integration

--- nginx_docker_revproxy/scripts/auth_engine.js
+++ nginx_docker_revproxy/scripts/auth_engine.js
@@ -1,8 +1,34 @@
 function authorize_operation(r) {
 
+    // storm-tape information parsing
+    let get_op_info = r.variables["request_uri"];
+    let method = r.variables["request_method"];
+    
+    let operation = (get_op_info.replace('/api/v1/', '')).split('/')[0];
+
+    if (operation === "stage") {
+        switch (method) {
+            case 'DELETE':
+                operation = "delete";
+                break;
+            case 'GET':
+                operation = "get_progress";
+                break;
+            case 'POST':
+                operation = "stage";
+                break;
+        }
+    } else if (operation === "archiveinfo") {
+        operation = (method === "POST") ? "archiveinfo" : "";
+    } else {
+        r.log("ERROR: UNRECOGNIZED OPERATION!");
+        r.return(403);
+        return;
+    }
+
     // dati da inviare a OPA
     let opa_data = {
-        "operation" : r.headersIn["X-Operation"],
+        "operation" : operation,
         "role" : "/" + r.headersIn["X-Role"],    // ho dovuto aggiungere lo slash siccome il token ha gruppo /dev 
         "uses_jwt" : r.headersIn["X-EnableJWT"], // quindi ora tutti i gruppi sono nella forma "/""
         "token" : r.headersIn["Authorization"]   // + nome gruppo.
